de83af99c496dd9f70dbd7af01e0902db4b17515
souvik0407999 <71004054+souvik0407999@users.noreply.github.com>
2020-09-09 21:41:42 +0530

    Add files via upload

---
+++ build/precache-manifest.0ce52a907420a994d416a36641a12191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b1cbf20885a351d4e4757b911cbe69",
+    "url": "/boardInfinity/index.html"
+  },
+  {
+    "revision": "d32eeaf783fedb7af4b7",
+    "url": "/boardInfinity/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0d8c05c449dd0d583940",
+    "url": "/boardInfinity/static/js/2.21d003cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/boardInfinity/static/js/2.21d003cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32eeaf783fedb7af4b7",
+    "url": "/boardInfinity/static/js/main.a7d281cc.chunk.js"
+  },
+  {
+    "revision": "e3032a57649ff30ec2c8",
+    "url": "/boardInfinity/static/js/runtime-main.b3472344.js"
+  }
+]);
